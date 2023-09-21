c5215f36a981ae38d6c0ce0a2e5b7b938ecddaad
Javier Gil Calvo <jgil@isdefe.es>
2023-09-21 15:00:58 +0200

    Prueba backend Vite

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// vite.config.js
 export default defineConfig({
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: './src/server/main.js',
-    },
-  },
-})
+  plugins: [react()],
+});
